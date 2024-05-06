ad67feebf06fa4fa3f9a9ce517cff688f1255b4e
Christiaan C Cupido <christiaanccupido@gmail.com>
2024-05-06 15:58:22 +0200

    .

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+           height="100vh"
+           projectID="6239c242-db40-4c6d-b320-8d083ad402c9"
+           userName="Admin"
+           userSecret="123123"
+
+        />
+    );
+}
+export default App;
